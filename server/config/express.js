c247cef239f7d85520d41984e89ca7671fb9e595
Chad Sheets <cjsheets@gmail.com>
2017-01-16 14:09:54 -0800

    configured to serve angular

--- server/config/express.js
+++ server/config/express.js
@@ -44,7 +44,7 @@ app.use(session({ secret: env.express.session_secret }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 /* -----------------------------------|
  *|  Routes
--- server/routes/index.js
+++ server/routes/index.js
@@ -34,23 +34,32 @@ router.use('/auth', auth);
 if(env.express.dev_routes) router.use('/dev', dev);
 
 // Return 404 for any undefined routes
-router.get(env.express.valid_routes, function(req, res, next) {
-   res.render('error.ejs', { message: req.flash('404, Page Not Found') });
-});
+// router.get(env.express.valid_routes, function(req, res, next) {
+//   debug('Invalid route attempted.');
+//   res.render('error.ejs', { message: req.flash('404, Page Not Found') });
+// });
 
 // Landing page
 router.get('/', function(req, res, next) {
+  debug('API root accessed. JSON response.');
   res.json({"apiRoot": true});
+});
+
+// Landing page
+router.get('/nl', function(req, res, next) {
+  debug('Route /nl accessed. Serving Angular site');
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
 /**
  * Anything else under '/', facilitates Angular HTML 5 routing. Must
  * declare below all other routes to avoid catching their requests
  */
-// router.get("/*", authHelper.isAuthOrRedirect, function(req, res, next) {
-//   res.sendFile(path.join(__dirname, '../', 'index.html'));
-//   //res.render('index');
-// });
+router.get('*', function(req, res, next) {
+  debug('Catch-All route accessed. Serving index');
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  //res.render('index');
+});
 
 debug('Routes initialized successfully');
 module.exports = router;
