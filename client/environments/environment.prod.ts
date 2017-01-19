f7c35f9e4f4943971d126e3bbccedf620e4c9b41
Chad Sheets <cjsheets@gmail.com>
2017-01-18 21:13:05 -0800

    adjusting angular env

--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  yelp: {
+    expires_in: 15551999,
+    token_type: 'Bearer',
+    access_token: process.env.YELP_ACCESS_TOKEN
+  }
 };
--- client/environments/index.js
+++ client/environments/index.js
@@ -2,4 +2,5 @@ var _      = require('lodash');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = _.merge(
+  {},
   require(`./${env}.env.js`) || require(`./${env}.js`) || {});
